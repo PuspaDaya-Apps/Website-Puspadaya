78570e91138e60e84fe655557cd3afd54fe86641
ajihermansya <ajihermansya14@gmail.com>
2025-02-02 08:24:48 +0700

    gap-card

--- src/components/Charts/GrafikPersebaranPosyandu.tsx
+++ src/components/Charts/GrafikPersebaranPosyandu.tsx
@@ -99,7 +99,7 @@ const GrafikPersebaranPosyandu: React.FC = () => {
 
   return (
     <div className="col-span-12 rounded-[10px] bg-white px-7.5 pt-7.5 shadow-1 dark:bg-gray-dark dark:shadow-card xl:col-span-5">
-      <div className="mb-4 justify-between gap-4 sm:flex">
+      <div className="mb-12 justify-between gap-4 sm:flex">
         <div>
         <div className="">
           <h1 className="text-body-2xlg font-bold text-dark dark:text-white">
